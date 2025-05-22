b4b5f3bbe4216840ad670a3e1ad820630a223861
Даниил Романович Рудометов <quincyque0@gmail.com>
2025-05-22 09:50:56 +0700

    Update manage.py

--- video/manage.py
+++ video/manage.py
@@ -1,1 +1,23 @@
-#Это файл который собственно и запускает проект на устройстве при помощи команды (python3 manage.py runserver)
+#Это файл который собственно и запускает проект на устройстве при помощи команды (python3 manage.py runserver) 
+# За частую он всегда стандартный при создании и не изменяется:
+"""Django's command-line utility for administrative tasks."""
+import os
+import sys
+
+
+def main():
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+    try:
+        from django.core.management import execute_from_command_line
+    except ImportError as exc:
+        raise ImportError(
+            "Couldn't import Django. Are you sure it's installed and "
+            "available on your PYTHONPATH environment variable? Did you "
+            "forget to activate a virtual environment?"
+        ) from exc
+    execute_from_command_line(sys.argv)
+
+
+if __name__ == '__main__':
+    main()
